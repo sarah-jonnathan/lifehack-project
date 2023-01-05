e03637792114df6b5e55fe235b7a84ccb2ce4d53
jogopin <jonn.g.pineda@gmail.com>
2023-01-05 19:39:45 +0100

    update user model

--- models/User.model.js
+++ models/User.model.js
@@ -20,6 +20,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    isAdmin:{
+      type:Boolean,
+      default:false
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
