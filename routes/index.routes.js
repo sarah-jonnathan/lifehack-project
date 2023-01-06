a885fe28d13a8b43e1722907cb825dcdc3cf6660
SarahK-coder <117637388+SarahK-coder@users.noreply.github.com>
2023-01-06 11:54:52 +0100

    Getting user details

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+// Require the User model in order to interact with the database
+const User = require("../models/User.model");
+
+// Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", {userInSession: req.session.currentUser});
 });
 
 module.exports = router;
