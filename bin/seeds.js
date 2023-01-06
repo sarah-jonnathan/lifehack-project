e4158b44754658c6c8ebac19ea82aa23bea9bda8
SarahK-coder <117637388+SarahK-coder@users.noreply.github.com>
2023-01-06 13:09:51 +0100

    Adding Tag model with some images

---
+++ bin/seeds.js
@@ -0,0 +1,61 @@
+// bin/seeds.js
+const mongoose = require('mongoose');
+const Tags = require('../models/Tag.model');
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/library-project" || "mongodb://localhost/library-project" || "mongodb://localhost:27017/library-project";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+const tags = [
+  {
+    name: "Parenting",
+    img: ["./images/tags/parenting01.jpg"]
+  },
+  {
+    name: "Food",
+    img: ["./images/tags/food01.jpg"]
+  },
+  {
+    name: "Cleaning",
+    img: ["./images/tags/cleaning01.jpg"]
+  },
+  {
+    name: "DIY",
+    img: ["./images/tags/diy01.jpg"]
+  },
+  {
+    name: "Animals",
+    img: ["./images/tags/animals01.jpg"]
+  },
+  {
+    name: "Tech",
+    img: ["./images/tags/tech01.jpg"]
+  },
+  {
+    name: "Miscellaneous",
+    img: ["./images/tags/misc01.jpg"]
+  }
+];
+
+
+const tagsPromise = Tags.create(tags);
+
+Promise.all([tagsPromise])
+  .then( (result) => {
+    const tagsCreated = result[0];
+    console.log(`Number of tags created... ${tagsCreated.length} `)
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+
+  })
+  .catch( e => console.log("error seeding data in DB....", e));
---
+++ models/Tag.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tagsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: [String],
+        required: true
+    }
+});
+
+const Tags = mongoose.model("Tags", tagsSchema); // create model Pizza
+
+module.exports = Tags;
