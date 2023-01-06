84aba9874c2f9759f412d5a68bf190c5a1231120
SarahK-coder <117637388+SarahK-coder@users.noreply.github.com>
2023-01-06 18:38:16 +0100

    Implemented Image uploading for Tags

--- routes/tag.routes.js
+++ routes/tag.routes.js
@@ -3,6 +3,27 @@ const Tag = require("../models/Tag.model");
 const Lifehack = require("../models/Lifehack.model")
 const User = require("../models/User.model");
 
+// ********* require fileUploader in order to use it *********
+const fileUploader = require('../config/cloudinary.config');
+
+/* Create a Tag */
+// GET route to display the form
+router.get('/tags/create', (req, res) => {
+    res.render("tags/tag-create");
+});
+// POST route to save a new tag to the database in the tags collection
+router.post('/tags/create', fileUploader.single('img'), (req, res, next) => {
+    const { name, img } = req.body;
+    const imgFilePath = req.file.path;
+    console.log(imgFilePath);
+    Tag.create({ name, img: imgFilePath })
+        .then(() => res.redirect('/tags'))
+        .catch(error => {
+            console.log("Error creating a new Tag in DB", error);
+            next(error);
+        });
+});
+
 //READ: List all tags
 router.get("/tags", (req, res, next) => {
     Tag.find()
