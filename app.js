88cf6d695cef383923319e258271427b9a0f8d7a
jogopin <jonn.g.pineda@gmail.com>
2023-01-09 11:19:00 +0100

    update lifehack-list view, admins can see edit and delete

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ hbs.registerHelper('equal', function(value1, value2, options) {
         }
     }
 });
+hbs.registerHelper('equalOrTrue', function(value1, value2,value3, options) {
+    if(value1 && value2 || value3){
+        
+        if( value3 || value1.toString()===value2.toString() ) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+    }
+});
 
 const app = express();
 
