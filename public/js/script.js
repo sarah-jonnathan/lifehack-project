00d2f29366d6fedf1f01da6484a5cb0d28dbcb47
jogopin <jonn.g.pineda@gmail.com>
2023-01-11 18:14:52 +0100

    add loading indicator

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,25 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("lifehack-project JS imported successfully!");
-});
+// // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("lifehack-project JS imported successfully!");
+// });
+
+const createBtnDomElem = document.getElementById("create")
+const loadingSignalDomElem = document.getElementById("loading")
+
+console.log
+
+
+//event listeners
+createBtnDomElem.addEventListener(`click`,()=>{
+  
+  loadingSignalDomElem.removeAttribute("hidden")
+  
+})
+
+// const selectElem = document.querySelector("select[name='tags']");
+// selectElem.addEventListener("change", function() {
+//   const selectedOptions = [...this.options].filter(option => option.selected);
+//   if (selectedOptions.length === 0) {
+//     this.options[0].selected = true;
+//   }
+// });
